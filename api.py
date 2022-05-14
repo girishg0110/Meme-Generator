a672d4f61125f886d4eb67701ed1bb5a85aa0ba3
Girish <choppat@Aarthys-iMac.local>
2022-05-14 19:27:13 -0400

    added meme sending functions

--- api.py
+++ api.py
@@ -8,26 +8,27 @@ account_sid, auth_token, number = load_twilio_config()
 client = Client(account_sid, auth_token)
 app = Flask(__name__)
 
-def get_meme(template_name, top_text, bottom_text):
-    url = f"http://apimeme.com/meme?meme={template_name}&top={top_text}&bottom={bottom_text}"
-    response = requests.request("GET", url, stream=True)
-    response.raw.decode_content = True
-    with open("img.jpg", "wb") as file:
-        shutil.copyfileobj(response.raw, file)
-    return response.raw
-
-def send_message(message, recipient):
+def send_text(recipient, message):
     client.messages.create(
         body=message,
         from_=number,
         to=recipient,
-        media_url="https://demo.twilio.com/owl.png"
     )
     return message
 
+def send_meme(recipient, meme_fields):
+    template_name, top_text, bottom_text = meme_fields
+    url = f"http://apimeme.com/meme?meme={template_name}&top={top_text}&bottom={bottom_text}"
+    client.messages.create(
+        from_=number,
+        to=recipient,
+        media_url=url
+    )
+    return url
+
+
 @app.route('/sms', methods=['POST'])
 def incoming():
-    # get_meme("10-Guy", "TOP", "BOTTOM")
     sender_number = request.form["From"]
     message = request.form["Body"]
 
